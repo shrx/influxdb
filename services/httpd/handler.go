105606e39c4f0c628e362d74c7ad93f4077a22de
shrx <differenxe@gmail.com>
2026-06-21 19:24:55 +0200

    Add support for flux profilers

--- services/httpd/handler.go
+++ services/httpd/handler.go
@@ -2257,6 +2257,13 @@ func (h *Handler) serveFluxQuery(w http.ResponseWriter, r *http.Request, user me
 		}
 		atomic.AddInt64(&h.stats.FluxQueryRequestBytesTransmitted, int64(n))
 		h.addQueryBytesForUser(userName, int64(n))
+
+		// Emit profiler result tables (profiler/query, profiler/operator) when
+        // the query enabled them via `option profiler.enabledProfilers`.
+        if presults, perr := q.ProfilerResults(); perr == nil && presults != nil {
+            pn, _ := encoder.Encode(w, presults)
+            atomic.AddInt64(&h.stats.FluxQueryRequestBytesTransmitted, int64(pn))
+        }
 	}
 }
 
